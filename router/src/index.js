b2237d6ef2e874ab384562651196a2974be75fd4
samwanyua <samexample8@gmail.com>
2024-02-13 03:56:42 +0300

    Native  Router

--- router/src/index.js
+++ router/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom'
-
+import {BrowserRouter} from 'react-router-native'
+// npm install react-router-native
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
